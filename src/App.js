9359ec16df70d6632172f6222300ff5ab4df0559
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-02-08 16:34:33 +0530

    Added Components to Face Recog App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import 'tachyons'
+import Particles from 'react-particles-js'
+
+const particleOptions = {
+    particles: {
+      line_linked: {
+        shadow: {
+          enable: true,
+          color: "#3CA9D1",
+          blur: 5
+        }
+      }
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className="particles" params={particleOptions}/>
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div className="white f3">
+		{"Maharshi your current rank is..."}
+		</div>
+	);
+}
+
+export default Rank;
